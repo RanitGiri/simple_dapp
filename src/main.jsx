d1a00c6a0c1c8ef2afaf9f2cb7c12cf8414af56d
RANIT <giriranit@gmail.com>
2025-09-24 23:47:28 +0530

    Connect to metamask fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { MetaMaskProvider } from "@metamask/sdk-react";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <MetaMaskProvider
+    debug={true}
+    sdkOptions={{
+      dappMetadata: {
+        name: "MyDapp",
+        url: window.location.href,
+      },
+    }}
+  >
     <App />
-  </StrictMode>,
+  </MetaMaskProvider>
 )
